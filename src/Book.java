bc79a84b94030cfcce7ddb6dd16407df194e99c1
MaximKartashov <maxkartashov5000@yandex.ru>
2022-08-28 18:23:01 +0300

    Homework 9.1

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,39 @@
 public class Book {
+    private String name;
+    private Author author;
+    private Integer publicationYear;
+
+    public Book(String name, Author author, Integer publicationYear) {
+        this.name = name;
+        this.author = author;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public Integer getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(Integer publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "name=" + name + '\'' +
+                ", author=" + author +
+                ", publicationYear=" + publicationYear +
+                '}';
+
+
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-
+        var author = new Author("Марк", "Лоуренс");
+        var author2 = new Author("Марк", "Лоуренс");
+        var author3 = new Author("Марк", "Лоуренс");
+        var book1 = new Book("Принц Терний", author, 2012);
+        var book2 = new Book("Король Терний", author, 2014);
+        var book3 = new Book("Император Терний", author, 2014);
+        System.out.println(author);
+        System.out.println(book1);
+        System.out.println(author2);
+        System.out.println(book2);
+        System.out.println(author3);
+        System.out.println(book3);
     }
 }
